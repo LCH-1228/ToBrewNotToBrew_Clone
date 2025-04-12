34bceef3d9c6198fcadcb9eb64e7b79a346cf709
LCH-1228 <chanho1228@gmail.com>
2025-04-12 13:57:31 +0900

    Initial project commit

---
+++ ToBrewNotToBrew_Clone/ToBrewNotToBrew_Clone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ToBrewNotToBrew_Clone
+//
+//  Created by Chanho Lee on 4/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ViewController is loaded")
+    }
+
+
+}
